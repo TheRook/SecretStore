bc7ecc69f85493ae4722bd695e37d2546af8d4f3
mike <mike@jetset.(none)>
2013-03-04 19:18:41 -0700

    new leveldb interface.

---
+++ store.h
@@ -0,0 +1,9 @@
+#ifndef STORE_H
+#define STORE_H
+
+leveldb_t * store_open(char * name);
+char * store_get(leveldb_t * store, char * key,size_t key_size, size_t *read_len);
+void store_put(leveldb_t * store, char * key, size_t key_size, char * value, size_t *value_len);
+void store_delete(leveldb_t * store, char * key, size_t key_size);
+
+#endif
